3d6fbb8b0f35c4493ac962254e246d9b0dc290ae
David Hufnagel <davehuf@Davids-MacBook-Pro-3.local>
2018-08-30 14:22:10 -0500

    small changes were made to the bash script based on suggestions from www.shellcheck.net.  Presumably these changes will make the script more stable.

--- SequelQC.sh
+++ SequelQC.sh
@@ -9,7 +9,7 @@ function print_help_menu() {
     cat <<helpChunk
     
     ###########################################################################
-                  This is the help menu for SequelQC version 0.9.9
+                  This is the help menu for SequelQC version 0.9.0
     ###########################################################################
     Dependencies for this program are samtools, Python, and R
     For this help menu use the parameter -h or no parameters
@@ -43,31 +43,31 @@ helpChunk
 
 #Set an array of files and line numbers to pass to R .  Arg 1=ARGS_FOR_R Arg 2 is I
 function make_args_for_R_array() {
-    if [ $GROUPS_DESIRED == "a" ]; then
+    if [ "$GROUPS_DESIRED" == "a" ]; then
         FILES_FOR_R+="$BASE.SMRTcellStats.txt,"
         FILES_FOR_R+="$BASE.readLens.sub.txt,"
         FILES_FOR_R+="$BASE.readLens.zmw.txt,"
         FILES_FOR_R+="$BASE.readLens.subedZmw.txt,"
         FILES_FOR_R+="$BASE.readLens.longSub.txt,"
         FILES_FOR_R+="$BASE.zmwStats.txt,"
-        LENGTHS_FOR_R+="$(wc -l $BASE.SMRTcellStats.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.readLens.sub.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.readLens.zmw.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.readLens.subedZmw.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.readLens.longSub.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.zmwStats.txt | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.SMRTcellStats.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.readLens.sub.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.readLens.zmw.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.readLens.subedZmw.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.readLens.longSub.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.zmwStats.txt" | cut -f 1 -d ' '),"
 
-    elif [ $GROUPS_DESIRED == "b"   ]; then
+    elif [ "$GROUPS_DESIRED" == "b"   ]; then
         FILES_FOR_R+="$BASE.SMRTcellStats.txt,"
         FILES_FOR_R+="$BASE.readLens.sub.txt,"
         FILES_FOR_R+="$BASE.readLens.subedZmw.txt,"
         FILES_FOR_R+="$BASE.readLens.longSub.txt,"
         FILES_FOR_R+="$BASE.zmwStats.txt,"
-        LENGTHS_FOR_R+="$(wc -l $BASE.SMRTcellStats.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.readLens.sub.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.readLens.subedZmw.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.readLens.longSub.txt | cut -f 1 -d ' '),"
-        LENGTHS_FOR_R+="$(wc -l $BASE.zmwStats.txt | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.SMRTcellStats.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.readLens.sub.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.readLens.subedZmw.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.readLens.longSub.txt" | cut -f 1 -d ' '),"
+        LENGTHS_FOR_R+="$(wc -l "$BASE.zmwStats.txt" | cut -f 1 -d ' '),"
     fi
 }
 
@@ -125,7 +125,7 @@ while getopts ":u:c:n:g:p:vkh" opt; do
         NTHREADS=$OPTARG
         ;;
       g )
-        if [ $OPTARG == "a" -o $OPTARG == "b" ]; then
+        if [ "$OPTARG" == "a" ] || [ "$OPTARG" == "b" ]; then
             GROUPS_DESIRED=$OPTARG
         else
             echo -e "\nERROR: Invalid Groups Desired Option: -g $OPTARG" 1>&2
@@ -133,7 +133,7 @@ while getopts ":u:c:n:g:p:vkh" opt; do
         fi
         ;;
       p )
-        if [ $OPTARG == "b" -o $OPTARG == "i" -o $OPTARG == "a" ]; then
+        if [ "$OPTARG" == "b" ] || [ "$OPTARG" == "i" ] || [ "$OPTARG" == "a" ]; then
             PLOTS_DESIRED=$OPTARG
         else
             echo -e "\nERROR: Invalid Plots Desired Option: -p $OPTARG" 1>&2
@@ -162,7 +162,7 @@ done
 
 
 #If required parameters are not provided throw an error and provide the help page
-if (( $REQUIRED_PAR != 2 )); then
+if (( REQUIRED_PAR != 2 )); then
     echo -e "\nERROR: The required parameters for this program are -u and -c."
     echo "You are lacking these parameters.  See our help page below"
     print_help_menu
@@ -175,20 +175,20 @@ I=1
 while read -r line; do
     SUBREADS_FILES_ARRAY_BAM[ $I ]="$line"
     (( I++ ))
-done < $SUBREADS_FILES_BAM
+done < "$SUBREADS_FILES_BAM"
 
 SCRAPS_FILES_ARRAY_BAM=()
 I=1
 while read -r line; do
     SCRAPS_FILES_ARRAY_BAM[ $I ]="$line"
     (( I++ ))
-done < $SCRAPS_FILES_BAM
+done < "$SCRAPS_FILES_BAM"
 
 
 #Go through BAM arrays and make arrays without the .bam at the end
 SUBREADS_FILES_ARRAY_NOBAM=()
 I=1
-for BAM in ${SUBREADS_FILES_ARRAY_BAM[@]}; do
+for BAM in "${SUBREADS_FILES_ARRAY_BAM[@]}"; do
     if [[ "$BAM" =~ (.*).bam ]]; then
         NOBAM=${BASH_REMATCH[1]}
         SUBREADS_FILES_ARRAY_NOBAM[ $I ]="$NOBAM"
@@ -198,7 +198,7 @@ done
 
 SCRAPS_FILES_ARRAY_NOBAM=()
 I=1
-for BAM in ${SCRAPS_FILES_ARRAY_BAM[@]}; do
+for BAM in "${SCRAPS_FILES_ARRAY_BAM[@]}"; do
     if [[ "$BAM" =~ (.*).bam ]]; then
         NOBAM=${BASH_REMATCH[1]}
         SCRAPS_FILES_ARRAY_NOBAM[ $I ]="$NOBAM"
@@ -210,7 +210,7 @@ done
 #Make an array of base filenames (before .scraps or .subreads)
 FILES_BASE_ARRAY=()
 I=1
-for NOBAM in ${SCRAPS_FILES_ARRAY_NOBAM[@]}; do
+for NOBAM in "${SCRAPS_FILES_ARRAY_NOBAM[@]}"; do
     if [[ "$NOBAM" =~ (.*).scraps ]]; then
         BASE=${BASH_REMATCH[1]}
         FILES_BASE_ARRAY[ $I ]="$BASE"
@@ -220,26 +220,26 @@ done
 
 
 #Extract names that will contain coords necessary for calculating length
-#FAILED_EXTRACTION="ERROR: BAM data extraction failed!"
+FAILED_EXTRACTION="ERROR: BAM data extraction failed!"
 if [ $VERBOSE == true ]; then
     echo "Extracting data from .bam files"
 fi
 
 I=1
-for BAM in ${SUBREADS_FILES_ARRAY_BAM[@]}; do
+for BAM in "${SUBREADS_FILES_ARRAY_BAM[@]}"; do
     NOBAM=${SUBREADS_FILES_ARRAY_NOBAM[I]}
-    samtools view --threads $NTHREADS -O SAM $BAM | awk '{print $1}' > $NOBAM.seqNames || {
-    echo >&2 $FAILED_EXTRACTION
+    samtools view --threads "$NTHREADS" -O SAM "$BAM" | awk '{print $1}' > "$NOBAM.seqNames" || {
+    echo >&2 "$FAILED_EXTRACTION"
     exit 1
     }
     (( I++ ))
 done
 
 I=1
-for BAM in ${SCRAPS_FILES_ARRAY_BAM[@]}; do
+for BAM in "${SCRAPS_FILES_ARRAY_BAM[@]}"; do
     NOBAM=${SCRAPS_FILES_ARRAY_NOBAM[I]}
-    samtools view --threads $NTHREADS -O SAM $BAM | awk '{print $1,"\t",$21,"\t",$22}' > $NOBAM.seqNamesPlus || {
-    echo >&2 $FAILED_EXTRACTION
+    samtools view --threads "$NTHREADS" -O SAM "$BAM" | awk '{print $1,"\t",$21,"\t",$22}' > "$NOBAM.seqNamesPlus" || {
+    echo >&2 "$FAILED_EXTRACTION"
     exit 1
     }
     (( I++ ))
@@ -255,15 +255,14 @@ fi
 FAILED_RLSTATS="ERROR: Calculation of read length statistics failed!"
 FILES_FOR_R=""
 LENGTHS_FOR_R=""
-L=12         #the number of inputs to pass to R (per scraps/subreads pair)
 I=1
-for SCRAPS_NOBAM in ${SCRAPS_FILES_ARRAY_NOBAM[@]}; do
+for SCRAPS_NOBAM in "${SCRAPS_FILES_ARRAY_NOBAM[@]}"; do
     SUBREADS_NOBAM=${SUBREADS_FILES_ARRAY_NOBAM[I]}
     BASE=${FILES_BASE_ARRAY[I]}
   
     if [ "$PY_VER" == 2 ]; then
-        python generateReadLenStats.py $SCRAPS_NOBAM.seqNamesPlus $SUBREADS_NOBAM.seqNames $BASE.SMRTcellStats.txt $BASE.readLens.sub.txt $BASE.readLens.zmw.txt $BASE.readLens.subedZmw.txt $BASE.readLens.longSub.txt $BASE.zmwStats.txt $GROUPS_DESIRED || {
-        echo >&2 $FAILED_RLSTATS
+        python generateReadLenStats.py "$SCRAPS_NOBAM.seqNamesPlus" "$SUBREADS_NOBAM.seqNames" "$BASE.SMRTcellStats.txt" "$BASE.readLens.sub.txt" "$BASE.readLens.zmw.txt" "$BASE.readLens.subedZmw.txt" "$BASE.readLens.longSub.txt" "$BASE.zmwStats.txt" "$GROUPS_DESIRED" || {
+        echo >&2 "$FAILED_RLSTATS"
         exit 1
         }
 
@@ -271,8 +270,8 @@ for SCRAPS_NOBAM in ${SCRAPS_FILES_ARRAY_NOBAM[@]}; do
         make_args_for_R_array
 
     elif [ "$PY_VER" == 3 ]; then
-        python generateReadLenStats_py3.py $SCRAPS_NOBAM.seqNamesPlus $SUBREADS_NOBAM.seqNames $BASE.SMRTcellStats.txt $BASE.readLens.sub.txt $BASE.readLens.zmw.txt $BASE.readLens.subedZmw.txt $BASE.readLens.longSub.txt $BASE.zmwStats.txt $GROUPS_DESIRED || {
-        echo >&2 $FAILED_RLSTATS
+        python generateReadLenStats_py3.py "$SCRAPS_NOBAM.seqNamesPlus" "$SUBREADS_NOBAM.seqNames" "$BASE.SMRTcellStats.txt" "$BASE.readLens.sub.txt" "$BASE.readLens.zmw.txt" "$BASE.readLens.subedZmw.txt" "$BASE.readLens.longSub.txt" "$BASE.zmwStats.txt" "$GROUPS_DESIRED" || {
+        echo >&2 "$FAILED_RLSTATS"
         exit 1
         }
 
@@ -282,7 +281,7 @@ for SCRAPS_NOBAM in ${SCRAPS_FILES_ARRAY_NOBAM[@]}; do
     else
         echo "ERROR: This program requires Python 2 or Python 3.  You seem to" 
         echo "be working with a different version."
-        echo >&2 $FAILED_RLSTATS
+        echo >&2 "$FAILED_RLSTATS"
         exit 1
     fi
 
@@ -296,7 +295,7 @@ fi
 
 
 #Make plots related to read length stats in R
-Rscript plotForSequelQC.R ${FILES_FOR_R::-1} ${LENGTHS_FOR_R::-1} $GROUPS_DESIRED $PLOTS_DESIRED $VERBOSE  #the '::-1' is to remove the comma at the end
+Rscript plotForSequelQC.R ${FILES_FOR_R::-1} ${LENGTHS_FOR_R::-1} "$GROUPS_DESIRED" "$PLOTS_DESIRED" "$VERBOSE"  #the '::-1' is to remove the comma at the end
 
 if [ $VERBOSE == true ]; then
     echo "Plot creation complete"
@@ -309,18 +308,18 @@ if [ $KEEP == false ]; then
         echo "Deleting intermediate files"
     fi
     I=1
-    for SCRAPS_NOBAM in ${SCRAPS_FILES_ARRAY_NOBAM[@]}; do
+    for SCRAPS_NOBAM in "${SCRAPS_FILES_ARRAY_NOBAM[@]}"; do
         SUBREADS_NOBAM=${SUBREADS_FILES_ARRAY_NOBAM[I]}
         BASE=${FILES_BASE_ARRAY[I]} 
 
-        rm $SCRAPS_NOBAM.seqNamesPlus
-        rm $SUBREADS_NOBAM.seqNames
-        rm $BASE.readLens.sub.txt
-        rm $BASE.readLens.zmw.txt
-        rm $BASE.readLens.longSub.txt
-        rm $BASE.readLens.subedZmw.txt
-        rm $BASE.SMRTcellStats.txt
-        rm $BASE.zmwStats.txt
+        rm "$SCRAPS_NOBAM.seqNamesPlus"
+        rm "$SUBREADS_NOBAM.seqNames"
+        rm "$BASE.readLens.sub.txt"
+        rm "$BASE.readLens.zmw.txt"
+        rm "$BASE.readLens.longSub.txt"
+        rm "$BASE.readLens.subedZmw.txt"
+        rm "$BASE.SMRTcellStats.txt"
+        rm "$BASE.zmwStats.txt"
 
         (( I++ ))
     done
